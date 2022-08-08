b9b4d803982bbcf3fc8ea740a8889b88658ab5ec
Leiniad <luis31lezama@hotmail.com>
2022-08-08 11:18:22 -0400

    Lista de tareas

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Container from './components/container';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App" >
+            <Container />
+        </div>
+    );
+}
+
+export default App;
